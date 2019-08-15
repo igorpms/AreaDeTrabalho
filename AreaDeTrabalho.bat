1079320226c4a811c9406acf76ded1ade60993f3
Lycia Kremer <32186405+LyciaKremer@users.noreply.github.com>
2019-08-15 10:22:51 -0300

    Add files via upload

---
+++ AreaDeTrabalho.bat
@@ -0,0 +1,5 @@
+start chrome.exe
+start Ssms.exe
+start C:\Users\lycia.kremer.ext\Downloads\eclipse-jee-2018-12-R-win32-x86_64\eclipse\eclipse.exe
+cd C:\Program Files (x86)\Fortinet\FortiClient
+start FortiClient.exe
