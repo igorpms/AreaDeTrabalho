2e7e686abd2c20df7804bf56235b3c6bc2951b90
Igor Marques Santiago <49252230+igorpms@users.noreply.github.com>
2019-08-15 10:28:03 -0300

    Adicionando uma linha para abrir o Slack tambérm

--- AreaDeTrabalho.bat
+++ AreaDeTrabalho.bat
@@ -1,5 +1,7 @@
 start chrome.exe
 start Ssms.exe
-start C:\Users\lycia.kremer.ext\Downloads\eclipse-jee-2018-12-R-win32-x86_64\eclipse\eclipse.exe
+start C:\Users\igor.santiago.ext\eclipse\jee-2019-06\eclipse\eclipse.exe
 cd C:\Program Files (x86)\Fortinet\FortiClient
 start FortiClient.exe
+cd C:\Users\igor.santiago.ext\AppData\Local\slack
+start Slack.exe
